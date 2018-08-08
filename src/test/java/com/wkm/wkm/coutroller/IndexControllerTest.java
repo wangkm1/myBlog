416f2503ccdb11dd6f5c3ff35c0496a5931495bb
wangkm <wangkmemail@163.com>
2018-08-08 23:17:30 +0800

    add test

---
+++ src/test/java/com/wkm/wkm/coutroller/IndexControllerTest.java
@@ -0,0 +1,13 @@
+package com.wkm.wkm.coutroller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    @Test
+    public void index() {
+
+    }
+}
